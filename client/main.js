4e9bd3d394ec60d0b67a02cf44196652fa196f41
Imeanstar <mike4404@naver.com>
2023-07-30 16:57:12 +0900

    7

--- client/main.js
+++ client/main.js
@@ -1,4 +1,26 @@
-console.log('hello js!');
+import { getNode, getStorage, setStorage } from './lib/index.js';
 
-console.log('hello');
-const a = 10;
+
+
+const textField = getNode('#textField');
+
+
+function handleTextField(){
+    const value = this.value;
+
+    setStorage('text',value);
+}
+
+
+function init(){
+
+    getStorage('text').then((res)=>{
+        textField.value = res;
+    })
+}
+
+
+
+textField.addEventListener('input',handleTextField);
+window.addEventListener('DOMContentLoaded', init);
+
